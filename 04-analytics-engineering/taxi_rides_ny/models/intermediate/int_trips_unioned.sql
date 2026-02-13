68515a520df0480f4188441d717cdc91f699e324
renelarsson <116158099+renelarsson@users.noreply.github.com>
2026-02-13 09:59:00 +0000

    Align trips union and update tests for hw4

--- 04-analytics-engineering/taxi_rides_ny/models/intermediate/int_trips_unioned.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/intermediate/int_trips_unioned.sql
@@ -1,3 +1,55 @@
-select * from {{ ref('stg_yellow_tripdata') }}
+with yellow as (
+	select
+		vendor_id,
+		pickup_datetime,
+		dropoff_datetime,
+		passenger_count,
+		trip_distance,
+		ratecode_id,
+		store_and_fwd_flag,
+		pickup_location_id,
+		dropoff_location_id,
+		payment_type,
+		fare_amount,
+		extra,
+		mta_tax,
+		tip_amount,
+		tolls_amount,
+		cast(null as float64) as ehail_fee,
+		improvement_surcharge,
+		total_amount,
+		cast(null as int64) as trip_type,
+		congestion_surcharge,
+		service_type
+	from {{ ref('stg_yellow_tripdata') }}
+),
+
+green as (
+	select
+		vendor_id,
+		pickup_datetime,
+		dropoff_datetime,
+		passenger_count,
+		trip_distance,
+		ratecode_id,
+		store_and_fwd_flag,
+		pickup_location_id,
+		dropoff_location_id,
+		payment_type,
+		fare_amount,
+		extra,
+		mta_tax,
+		tip_amount,
+		tolls_amount,
+		ehail_fee,
+		improvement_surcharge,
+		total_amount,
+		trip_type,
+		congestion_surcharge,
+		service_type
+	from {{ ref('stg_green_tripdata') }}
+)
+
+select * from yellow
 union all
-select * from {{ ref('stg_green_tripdata') }}
+select * from green
